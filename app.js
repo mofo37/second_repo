61927d9a50455067fd8c70005f8518dab607d2e9
Morgan Fogarty <mofo37@gmail.com>
2017-02-08 13:58:37 -0800

    Added extra variable usability in js

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 'use strict';
 
-alert('Guess some stuff about me!');
+var userName = prompt('What is your name?');
+
+console.log(userName);
+
+alert('Hi, ' + userName + ', guess some stuff about me!');
 
 var firstAnswer = prompt('Do I like books?', 'Yes or No');
 
 var firstAnswerLowerCase = firstAnswer.toLowerCase();
 
 if (firstAnswerLowerCase === 'yes' || firstAnswerLowerCase === 'y') {
-    alert('Heck yeah, I do!');
-    console.log('Yes');
+  alert('Heck yeah, I do!');
+  console.log('Yes');
 } else if (firstAnswerLowerCase === 'no' || firstAnswerLowerCase === 'n') {
-    alert('What kind of a monster do you think I am?')
-    console.log('No');
+  alert('What kind of a monster do you think I am?');
+  console.log('No');
 } else {
-    alert('acting coy, eh?');
-    console.log('Something weird.');
+  alert('acting coy, eh?');
+  console.log('Something weird.');
 }
 
 var secondAnswer = prompt('Do I love potatoes?', 'Yes or No');
@@ -22,14 +26,14 @@ var secondAnswer = prompt('Do I love potatoes?', 'Yes or No');
 var secondAnswerLowerCase = secondAnswer.toLowerCase();
 
 if (secondAnswerLowerCase === 'yes' || secondAnswerLowerCase === 'y') {
-    alert('My ancestors migrated to the United States during the great potato famine, so I feel sad for them... but potatoes are DELICIOUS, so you are right.');
-    console.log('Yes');
+  alert('My ancestors migrated to the United States during the great potato famine, so I feel sad for them... but potatoes are DELICIOUS, so you are right.');
+  console.log('Yes');
 } else if (secondAnswerLowerCase === 'no' || secondAnswerLowerCase === 'n') {
-    alert('Who do you think I am?! Of course I like them!');
-    console.log('No');
+  alert('Who do you think I am?! Of course I like them!');
+  console.log('No');
 } else {
-    alert('Let us try a different question');
-    console.log('Something else weird.');
+  alert('Let us try a different question');
+  console.log('Something else weird.');
 }
 
 var thirdAnswer = prompt('Do I often pet cats?', 'Yes or No');
@@ -37,14 +41,14 @@ var thirdAnswer = prompt('Do I often pet cats?', 'Yes or No');
 var thirdAnswerLowerCase = thirdAnswer.toLowerCase();
 
 if (thirdAnswerLowerCase === 'yes' || thirdAnswerLowerCase === 'y') {
-    alert('I often meander around Portland, looking for cats to pet');
-    console.log('Yes');
+  alert('I often meander around Portland, looking for cats to pet');
+  console.log('Yes');
 } else if (thirdAnswerLowerCase === 'no' || thirdAnswerLowerCase === 'n') {
-    alert('You really do not understand me.');
-    console.log('No');
+  alert(userName + ', you really do not understand me.');
+  console.log('No');
 } else {
-    alert('You are being weird.');
-    console.log('Something even weirder.');
+  alert('You are being weird.');
+  console.log('Something even weirder.');
 }
 
 var fourthAnswer = prompt('Are cats better than dogs?', 'Yes or No');
@@ -52,14 +56,14 @@ var fourthAnswer = prompt('Are cats better than dogs?', 'Yes or No');
 var fourthAnswerLowerCase = fourthAnswer.toLowerCase();
 
 if (fourthAnswerLowerCase === 'yes' || fourthAnswerLowerCase === 'y') {
-    alert('I am actually partial to both.');
-    console.log('Yes');
+  alert('Well, ' + userName + ', I am actually partial to both.');
+  console.log('Yes');
 } else if (fourthAnswerLowerCase === 'no' || fourthAnswerLowerCase === 'n') {
-    alert('You are right. They are both fluffy, beautiful creatures with different things to offer.');
-    console.log('No');
+  alert('You are right. They are both fluffy, beautiful creatures with different things to offer.');
+  console.log('No');
 } else {
-    alert('Yes or No next time, bud.');
-    console.log('Oh, geez.');
+  alert('Yes or No next time, bud.');
+  console.log('Oh, geez.');
 }
 
 var fifthAnswer = prompt('Do you know who I am yet?', 'Yes or No');
@@ -67,12 +71,12 @@ var fifthAnswer = prompt('Do you know who I am yet?', 'Yes or No');
 var fifthAnswerLowerCase = fifthAnswer.toLowerCase();
 
 if (fifthAnswerLowerCase === 'yes' || fifthAnswerLowerCase === 'y') {
-    alert('Yay!');
-    console.log('Yes');
+  alert('Yay!');
+  console.log('Yes');
 } else if (fifthAnswerLowerCase === 'no' || fifthAnswerLowerCase === 'n') {
-    alert('Think harder.');
-    console.log('No');
+  alert('Think harder.');
+  console.log('No');
 } else {
-    alert('I wonder what we could have had.');
-    console.log('Another weird answer');
+  alert('I wonder what we could have had.');
+  console.log('Another weird answer');
 }
