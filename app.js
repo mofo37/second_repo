9800140328dff6b85eb7f7f02725a0e7c183a91e
Morgan Fogarty <mofo37@gmail.com>
2017-02-08 16:26:34 -0800

    Added content to HTML, added while loop to js

--- app.js
+++ app.js
@@ -1,82 +1,105 @@
 'use strict';
 
-var userName = prompt('What is your name?');
+// var userName = prompt('What is your name?');
 
-console.log(userName);
+// console.log(userName);
 
-alert('Hi, ' + userName + ', guess some stuff about me!');
+// alert('Hi, ' + userName + ', guess some stuff about me!');
 
-var firstAnswer = prompt('Do I like books?', 'Yes or No');
+// var firstAnswer = prompt('Have I ever lived on wheels?', 'Yes or No');
 
-var firstAnswerLowerCase = firstAnswer.toLowerCase();
+// var firstAnswerLowerCase = firstAnswer.toLowerCase();
 
-if (firstAnswerLowerCase === 'yes' || firstAnswerLowerCase === 'y') {
-  alert('Heck yeah, I do!');
-  console.log('Yes');
-} else if (firstAnswerLowerCase === 'no' || firstAnswerLowerCase === 'n') {
-  alert('What kind of a monster do you think I am?');
-  console.log('No');
-} else {
-  alert('acting coy, eh?');
-  console.log('Something weird.');
-}
+// if (firstAnswerLowerCase === 'yes' || firstAnswerLowerCase === 'y') {
+//   alert('You are correct! I lived in a 1987 Volkswagen camper van with my boyfriend from June 2015 until January 2017. We travelled around the perimeter of the United States. It was an incredible experience, but I am happy to be settling down in Portland for awhile.');
+//   console.log('Yes');
+// } else if (firstAnswerLowerCase === 'no' || firstAnswerLowerCase === 'n') {
+//   alert('Wrong! I lived in a 1987 Volkswagen camper van with my boyfriend from June 2015 until January 2017. We travelled around the perimeter of the United States. It was an incredible experience, but I am happy to be settling down in Portland for awhile');
+//   console.log('No');
+// } else {
+//   alert('Let us try another.');
+//   console.log('Something weird.');
+// }
 
-var secondAnswer = prompt('Do I love potatoes?', 'Yes or No');
+// var secondAnswer = prompt('Do I love potatoes?', 'Yes or No');
 
-var secondAnswerLowerCase = secondAnswer.toLowerCase();
+// var secondAnswerLowerCase = secondAnswer.toLowerCase();
 
-if (secondAnswerLowerCase === 'yes' || secondAnswerLowerCase === 'y') {
-  alert('My ancestors migrated to the United States during the great potato famine, so I feel sad for them... but potatoes are DELICIOUS, so you are right.');
-  console.log('Yes');
-} else if (secondAnswerLowerCase === 'no' || secondAnswerLowerCase === 'n') {
-  alert('Who do you think I am?! Of course I like them!');
-  console.log('No');
-} else {
-  alert('Let us try a different question');
-  console.log('Something else weird.');
-}
+// if (secondAnswerLowerCase === 'yes' || secondAnswerLowerCase === 'y') {
+//   alert('My ancestors migrated to the United States during the great potato famine around 1850. It was a rough time for them... but potatoes are DELICIOUS, so you are right. I do love them.');
+//   console.log('Yes');
+// } else if (secondAnswerLowerCase === 'no' || secondAnswerLowerCase === 'n') {
+//   alert('My ancestors migrated to the United States during the great potato famine around 1850. It was a rough time for them... but potatoes are DELICIOUS, so you are wrong. I love them. ');
+//   console.log('No');
+// } else {
+//   alert('On to the next one!');
+//   console.log('Something else weird.');
+// }
 
-var thirdAnswer = prompt('Do I often pet cats?', 'Yes or No');
+// var thirdAnswer = prompt('Do I often pet cats?', 'Yes or No');
 
-var thirdAnswerLowerCase = thirdAnswer.toLowerCase();
+// var thirdAnswerLowerCase = thirdAnswer.toLowerCase();
 
-if (thirdAnswerLowerCase === 'yes' || thirdAnswerLowerCase === 'y') {
-  alert('I often meander around Portland, looking for cats to pet');
-  console.log('Yes');
-} else if (thirdAnswerLowerCase === 'no' || thirdAnswerLowerCase === 'n') {
-  alert(userName + ', you really do not understand me.');
-  console.log('No');
-} else {
-  alert('You are being weird.');
-  console.log('Something even weirder.');
-}
+// if (thirdAnswerLowerCase === 'yes' || thirdAnswerLowerCase === 'y') {
+//   alert('Yes, ' + userName + ', I often meander around Portland, looking for cats to pet');
+//   console.log('Yes');
+// } else if (thirdAnswerLowerCase === 'no' || thirdAnswerLowerCase === 'n') {
+//   alert(userName + ', you really do not understand me.');
+//   console.log('No');
+// } else {
+//   alert('Come on, everyone loves cats.');
+//   console.log('Something even weirder.');
+// }
 
-var fourthAnswer = prompt('Are cats better than dogs?', 'Yes or No');
+// var fourthAnswer = prompt('Do I have any siblings?', 'Yes or No');
 
-var fourthAnswerLowerCase = fourthAnswer.toLowerCase();
+// var fourthAnswerLowerCase = fourthAnswer.toLowerCase();
 
-if (fourthAnswerLowerCase === 'yes' || fourthAnswerLowerCase === 'y') {
-  alert('Well, ' + userName + ', I am actually partial to both.');
-  console.log('Yes');
-} else if (fourthAnswerLowerCase === 'no' || fourthAnswerLowerCase === 'n') {
-  alert('You are right. They are both fluffy, beautiful creatures with different things to offer.');
-  console.log('No');
-} else {
-  alert('Yes or No next time, bud.');
-  console.log('Oh, geez.');
-}
+// if (fourthAnswerLowerCase === 'yes' || fourthAnswerLowerCase === 'y') {
+//   alert('Well, ' + userName + ', I am the youngest of 5 kids, so you are right!');
+//   console.log('Yes');
+// } else if (fourthAnswerLowerCase === 'no' || fourthAnswerLowerCase === 'n') {
+//   alert('You are incorrect. I am the youngest of 5 kids.');
+//   console.log('No');
+// } else {
+//   alert('Yes or No next time, bud.');
+//   console.log('Something unexpected.');
+// }
 
-var fifthAnswer = prompt('Do you know who I am yet?', 'Yes or No');
+// var fifthAnswer = prompt('Is computer programming my second career?', 'Yes or No');
 
-var fifthAnswerLowerCase = fifthAnswer.toLowerCase();
+// var fifthAnswerLowerCase = fifthAnswer.toLowerCase();
 
-if (fifthAnswerLowerCase === 'yes' || fifthAnswerLowerCase === 'y') {
-  alert('Yay!');
-  console.log('Yes');
-} else if (fifthAnswerLowerCase === 'no' || fifthAnswerLowerCase === 'n') {
-  alert('Think harder.');
-  console.log('No');
-} else {
-  alert('I wonder what we could have had.');
-  console.log('Another weird answer');
-}
+// if (fifthAnswerLowerCase === 'yes' || fifthAnswerLowerCase === 'y') {
+//   alert('You are right! My first career was as a dancer. After college I danced professionally in modern dance companies in New York City, then was a professor of dance at Northern Illinois University. I see a lot of similarities between dance and programming, actually!');
+//   console.log('Yes');
+// } else if (fifthAnswerLowerCase === 'no' || fifthAnswerLowerCase === 'n') {
+//   alert('Incorrect. My first career was as a dancer. After college I danced professionally in modern dance companies in New York City, then was a professor of dance at Northern Illinois University. I see a lot of similarities between dance and programming, actually!');
+//   console.log('No');
+// } else {
+//   alert('Nice try!');
+//   console.log('Another weird answer');
+// }
+
+
+var numberAnswer = 37;
+var counter = 0;
+
+while (counter < 4 && numberGuess !== numberAnswer) {
+  var numberGuess = prompt('Guess a number 1 to 100'); 
+  counter++;
+  if (numberGuess > numberAnswer) {
+    alert('Too high!');
+    console.log('Too high');
+  } else if (numberGuess < numberAnswer) {
+    alert('Too low.');
+    console.log('Too low.');
+  } else {
+    alert('You are correct! Wow!');
+    numberGuess = numberAnswer;
+  }
+};
+
+
+
+
